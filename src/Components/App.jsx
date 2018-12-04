b0d325a5bc95a87294f1737d9f90dd97f747d58b
Patrick Hogan <phogan110@live.com>
2018-12-03 22:57:14 -0700

    added `button` that toggles `hasLoaded` and pasted `Greeting` above App

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
-import Greeting from "./Greeting";
+
+class Greeting extends Component {
+  render() {
+    const text = "Welcome to 'Reacting to React'";
+    return (
+      <h1>
+        Heya {this.props.name}! {text}
+      </h1>
+    );
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: "This is an input",
+      hasLoaded: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    this.setState({
+      hasLoaded: true
+    });
+  }
+
   render() {
-    return <Greeting name="Patrick" />;
+    return (
+      <>
+        <input type="text" placeholder={this.state.inputText} />
+        <button onClick={this.handleClick}>Button</button>
+        <Greeting name="Patrick" />
+      </>
+    );
   }
 }
 
