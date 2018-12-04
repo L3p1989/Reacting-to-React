589401f7f098783e2901f7bc605b0c1084df93ac
Patrick Hogan <phogan110@live.com>
2018-12-03 22:35:31 -0700

    changed `Greeting` to class based component

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Greeting = props => {
-  return <h1>Heya {props.name}!</h1>;
-};
+// const Greeting = props => {
+//   return <h1>Heya {props.name}!</h1>;
+// };
+
+class Greeting extends Component {
+  render() {
+    return <h1>Heya {this.props.name}!</h1>;
+  }
+}
 
 export default Greeting;
